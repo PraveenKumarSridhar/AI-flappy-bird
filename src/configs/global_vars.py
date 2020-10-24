fa6caf592737908667b729a1ee31d51225836501
PraveenKumarSridhar <prasri.pk@gmail.com>
2020-10-25 02:07:49 +0530

    cove major config to gobal varfile

--- src/configs/global_vars.py
+++ src/configs/global_vars.py
@@ -1,2 +1,19 @@
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
+
+import pygame,os
+
+# load the bird images.
+BIRDS_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("src","imgs","bird1.png"))),\
+    pygame.transform.scale2x(pygame.image.load(os.path.join("src","imgs","bird2.png"))),\
+        pygame.transform.scale2x(pygame.image.load(os.path.join("src","imgs","bird3.png")))]
+# load the pipe image.
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("src","imgs","pipe.png")))
+# load the base image.
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("src","imgs","base.png")))
+# load the background image.
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("src","imgs","bg.png")))
+
+
+pygame.font.init()
+STAT_FONT = pygame.font.SysFont("Comicsans",50)
